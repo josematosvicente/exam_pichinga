3c8698dae723724b91ac6653b5e2782a5457ef91
Ivan Quiñonez <ivanqcotera@gmail.com>
2022-09-09 11:50:45 -0500

    archivos yml de config para msvc-financial, msvc-product, msvc-administrative v.1.0

--- msvc-client/src/main/java/com/bootcamp/springwebflux/msvcclient/MsvcClientApplication.java
+++ msvc-client/src/main/java/com/bootcamp/springwebflux/msvcclient/MsvcClientApplication.java
@@ -6,11 +6,13 @@ import com.bootcamp.springwebflux.msvcclient.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 import reactor.core.publisher.Flux;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class MsvcClientApplication{
 
 	public static void main(String[] args) {
