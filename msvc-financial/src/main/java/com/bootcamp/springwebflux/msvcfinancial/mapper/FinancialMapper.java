97c73ecb55700eb9dd2bae556dce63186d7c2e4e
Jose Matos <jose.jona.mv@gmail.com>
2022-09-13 18:38:23 -0500

    Changes

--- msvc-financial/src/main/java/com/bootcamp/springwebflux/msvcfinancial/mapper/FinancialMapper.java
+++ msvc-financial/src/main/java/com/bootcamp/springwebflux/msvcfinancial/mapper/FinancialMapper.java
@@ -2,7 +2,12 @@ package com.bootcamp.springwebflux.msvcfinancial.mapper;
 
 import org.mapstruct.Mapper;
 
+import com.msvc.specification.api.dto.NewAccountProductDto;
+import com.msvc.specification.api.dto.NewProductDto;
+
 @Mapper(componentModel = "spring")
 public interface FinancialMapper {
+
+    NewProductDto toNewProductDto(NewAccountProductDto value);
     
 }
