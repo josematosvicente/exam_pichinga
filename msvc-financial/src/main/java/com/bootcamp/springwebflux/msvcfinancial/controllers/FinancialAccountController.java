e1784308691cf417ba284a35ddbba47ea282e9b0
Jose Matos <jose.jona.mv@gmail.com>
2022-09-23 19:35:25 -0500

    Add

--- msvc-financial/src/main/java/com/bootcamp/springwebflux/msvcfinancial/controllers/FinancialAccountController.java
+++ msvc-financial/src/main/java/com/bootcamp/springwebflux/msvcfinancial/controllers/FinancialAccountController.java
@@ -4,14 +4,16 @@ import java.net.URI;
 
 import javax.validation.Valid;
 
-import com.bootcamp.springwebflux.msvcfinancial.services.FinancialAccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ServerWebExchange;
 
 import com.bootcamp.springwebflux.msvcfinancial.mapper.FinancialMapper;
+import com.bootcamp.springwebflux.msvcfinancial.producer.TopicProducer;
+import com.bootcamp.springwebflux.msvcfinancial.services.FinancialAccountService;
 import com.msvc.specification.api.FinantialApi;
 import com.msvc.specification.api.dto.AccountDto;
 import com.msvc.specification.api.dto.MovementDto;
@@ -26,6 +28,9 @@ public class FinancialAccountController implements FinantialApi {
 
     @Autowired
     private FinancialMapper financialMapper;
+    
+    @Autowired
+    private TopicProducer topicProducer;
 
     @Override
     public Mono<ResponseEntity<AccountDto>> balanceAccount(final String arg0,
@@ -46,6 +51,7 @@ public class FinancialAccountController implements FinantialApi {
     public Mono<ResponseEntity<AccountDto>> movement(final String id,
         final @Valid Mono<MovementDto> movementDto,
             final ServerWebExchange arg2) {
+                topicProducer.send("Mensagem de teste enviada ao tópico");
         return movementDto.flatMap(movement -> financialAccountService.save(id, financialMapper.toModel(movement))
             .map(account ->
                 ResponseEntity.created(URI.create("/administrative/accounts/".concat(id)))
@@ -60,4 +66,5 @@ public class FinancialAccountController implements FinantialApi {
         // TODO Auto-generated method stub
         return null;
     }
+
 }
